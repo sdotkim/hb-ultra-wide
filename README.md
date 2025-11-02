f99c7fe2eb4d54a28dd51620b70d7995e33d7baa
Stanislav Kim <stanislav.kim@gmail.com>
2025-11-02 18:45:10 -0500

    It's alive

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Move cursor horisontally on ultrawide monitor
     - captures hotkey
     - gathers display info and current cursor position
     - invokes ydotool command
+
+
+NOTE: disable mouse acceleration in gnome settings to prevent mouse cursor jumping eratically.
+
+
+### Roadmap
+1. Add prefs UI to configure keyboard shortcut
+    - check gnome global keyboard shortcut to prevent overlap
--- extension/extension.js
+++ extension/extension.js
@@ -7,9 +7,15 @@ import * as Main from "resource:///org/gnome/shell/ui/main.js";
 // import Util from "resource:///org/gnome/shell/util.js";
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
+let home = GLib.get_home_dir();
+let ydotoolPath = `${home}/.local/bin/ydotool`;
+
 export default class UltraWide extends Extension {
     #settings;
     #scrollSignal;
+    #zones;
+    #mh;
+    #mw;
 
     // Lazy-load settings
     get settings() {
@@ -21,6 +27,12 @@ export default class UltraWide extends Extension {
 
     enable() {
         if (!Main.wm) return;
+
+        const monitor = global.display.get_primary_monitor();
+        let {x: mx, y:my, width: mw, height: mh} = global.display.get_monitor_geometry(monitor);
+        this.#mh = mh;
+        this.#mw = mw;
+        this.#zones = this.settings.get_int("display-zones");
 
         // Register keybinding
         Main.wm.addKeybinding(
@@ -58,21 +70,49 @@ export default class UltraWide extends Extension {
         }
     }
 
-    handleScroll(event) {
-        const delta = 50;
-        const direction = event.get_scroll_direction();
+    handleScroll(actor, event) {
+        if (event.type() !== Clutter.EventType.SCROLL) return Clutter.EVENT_STOP;
 
-        if (direction === Clutter.ScrollDirection.UP) {
-            this.moveCursor(delta, 0);
-        } else if (direction === Clutter.ScrollDirection.DOWN) {
-            this.moveCursor(-delta, 0);
-        }
+        const scrollDirection = event.get_scroll_direction()
+        if (scrollDirection == Clutter.ScrollDirection.SMOOTH) return Clutter.EVENT_STOP;
+
+        const position = this.getNewPosition(scrollDirection? -1: 1)
+        this.moveCursor(...position);
 
         return Clutter.EVENT_STOP;
     }
 
+    getNewPosition(direction) {
+        let [cx, cy] = global.get_pointer();
+
+        // compute zone sizes
+        const zoneWidth = Math.floor(this.#mw / this.#zones);
+
+        // detect current zone
+        let currentZone = Math.floor(cx / zoneWidth) + 1;
+
+        // compute target zone
+        let newZone = currentZone + direction;
+        newZone = Math.max(1, Math.min(this.#zones, newZone));
+
+        // compute target absolute coordinate
+        const targetX = Math.floor((newZone - 1) * zoneWidth + zoneWidth / 2);
+        const targetY = Math.floor(this.#mh / 2);
+
+        // convert to relative movement for ydotool
+        const dx = targetX - cx;
+        const dy = targetY - cy;
+
+        return [dx, dy];
+    }
+
     moveCursor(dx, dy) {
-        // Use GLib to spawn the external tool
-        GLib.spawn_command_line_async(`ydotool mousemove_relative ${dx.toString()} ${dy.toString()}`);
+        GLib.spawn_async(
+            null,
+            [ydotoolPath, 'mousemove', '-x', `${dx.toString()}`, '-y', `${dy.toString()}`],
+            null,
+            GLib.SpawnFlags.SEARCH_PATH,
+            null
+        );
     }
 }
